b221dcec163f79033738a2baad491782b6735159
Kadir <83705620+Kadirsol@users.noreply.github.com>
2021-07-20 23:12:43 +0300

    Create Interfaces.sol

---
+++ Multisig Wallet/MultisigERC20/contract/Interfaces.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function name() external view   returns (string memory);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+}
