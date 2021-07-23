a9f04f7be33d90937062c403a58c3a6610b7fd01
Kadir <83705620+Kadirsol@users.noreply.github.com>
2021-07-23 23:30:58 +0300

    Create Interfaces.sol

---
+++ DEFI/Uniswap Examples/Liquidity/contracts/Interfaces.sol
@@ -0,0 +1,52 @@
+  pragma solidity 0.8.0;
+
+interface IUniswapV2Router {
+function addLiquidity(
+  address tokenA,
+  address tokenB,
+  uint amountADesired,
+  uint amountBDesired,
+  uint amountAMin,
+  uint amountBMin,
+  address to,
+  uint deadline
+) external returns (uint amountA, uint amountB, uint liquidity);
+
+function removeLiquidity(
+  address tokenA,
+  address tokenB,
+  uint liquidity,
+  uint amountAMin,
+  uint amountBMin,
+  address to,
+  uint deadline
+) external returns (uint amountA, uint amountB);
+
+function addLiquidityETH(
+  address token,
+  uint amountTokenDesired,
+  uint amountTokenMin,
+  uint amountETHMin,
+  address to,
+  uint deadline
+) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+
+function removeLiquidityETH(
+  address token,
+  uint liquidity,
+  uint amountTokenMin,
+  uint amountETHMin,
+  address to,
+  uint deadline
+) external returns (uint amountToken, uint amountETH);
+} 
+
+interface UniswapV2Factory {
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+}
+
+interface IERC20 {    
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
